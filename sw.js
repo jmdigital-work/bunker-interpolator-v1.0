7b5b8586137a9ff8cc1e8cbdf3e69a8bfa68d1a5
jmdigital-work <johnmarkdigital.work@gmail.com>
2026-08-31 13:41:51 +0800

    Update MarineCalc PWA branding and icons

--- sw.js
+++ sw.js
@@ -1,46 +1,81 @@
-const CACHE_NAME = "bunker-interpolator-v3";
+const CACHE_NAME = "marinecalc-v1";
+
 const APP_SHELL = [
   "./",
   "./index.html",
   "./style.css",
-  "./script.js",
   "./manifest.webmanifest",
-  "./icons/icon-192.png",
-  "./icons/icon-512.png"
+
+  "./icons/marinecalc-favicon-32.png",
+  "./icons/marinecalc-icon-192.png",
+  "./icons/marinecalc-icon-512.png"
 ];
 
+
 self.addEventListener("install", (event) => {
+
   event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => cache.addAll(APP_SHELL))
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(APP_SHELL);
+    })
   );
+
   self.skipWaiting();
+
 });
 
+
 self.addEventListener("activate", (event) => {
+
   event.waitUntil(
-    caches.keys().then((keys) =>
-      Promise.all(
+    caches.keys().then((keys) => {
+
+      return Promise.all(
+
         keys
           .filter((key) => key !== CACHE_NAME)
           .map((key) => caches.delete(key))
-      )
-    )
-  );
-  self.clients.claim();
-});
 
-self.addEventListener("fetch", (event) => {
-  if (event.request.method !== "GET") return;
+      );
 
-  event.respondWith(
-    caches.match(event.request).then((cached) => {
-      if (cached) return cached;
-
-      return fetch(event.request).then((response) => {
-        const copy = response.clone();
-        caches.open(CACHE_NAME).then((cache) => cache.put(event.request, copy));
-        return response;
-      });
     })
   );
+
+  self.clients.claim();
+
+});
+
+
+self.addEventListener("fetch", (event) => {
+
+  if (event.request.method !== "GET") return;
+
+
+  event.respondWith(
+
+    caches.match(event.request).then((cached) => {
+
+      if (cached) {
+        return cached;
+      }
+
+
+      return fetch(event.request).then((response) => {
+
+        const copy = response.clone();
+
+        caches.open(CACHE_NAME).then((cache) => {
+
+          cache.put(event.request, copy);
+
+        });
+
+        return response;
+
+      });
+
+    })
+
+  );
+
 });
